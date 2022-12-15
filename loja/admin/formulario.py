f45b9988b07d71b6ebeafd4703c98828eb1c4f2d
JhonneAmorim <jhonneamorimdev@gmail.com>
2022-12-15 09:44:26 -0300

    login, messangens de alertas e rotas novas

--- loja/admin/formulario.py
+++ loja/admin/formulario.py
@@ -2,11 +2,17 @@ from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 class RegistrationForm(Form):
     name = StringField('nome', [validators.Length(min=4, max=25)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
+    username = StringField('Usuario', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=35)])
+    password = PasswordField('Senha', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
+        validators.EqualTo('confirm', message='Senha e confirmação não são iguais')
     ])
-    confirm = PasswordField('Repeat Password')
-   
+    confirm = PasswordField('Repita a sua senha:')
+
+
+
+class LoginFormulario(Form):
+    email = StringField('Email', [validators.Length(min=6, max=35)])
+    password = PasswordField('Senha', [validators.DataRequired()])
+        
--- loja/admin/rotas.py
+++ loja/admin/rotas.py
@@ -1,14 +1,17 @@
 from flask import *
 
 from loja import app, db, bcrypt
-from .formulario import RegistrationForm
+from .formulario import RegistrationForm, LoginFormulario
 from .models import User
 import os
 
 
-@app.route('/')
-def home():
-  return "seja bem vindo ao sistema em flask"
+@app.route('/admin')
+def admin():
+  if 'email' not in session:
+    flash(f'Por favor fazer seu login primeiro', 'warning')
+    return redirect(url_for('login'))
+  return render_template('admin/index.html', title='Pagina Administrativa')
 
 @app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
@@ -16,8 +19,22 @@ def registrar():
     if request.method == 'POST' and form.validate():
         hash_password = bcrypt.generate_password_hash(form.password.data)
         user = User(name = form.name.data, username = form.username.data, email = form.email.data,
-        password = hash_password, profile = form.username.data)
+        password = hash_password)
         db.session.add(user)
-        flash('Obrigado por se registrar')
+        db.session.commit()
+        flash(f'Obrigado {form.name.data} por se registrar','success')
         return redirect(url_for('login'))
     return render_template('admin/registrar.html', title="Pagina de Registros", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  form = LoginFormulario(request.form)
+  if request.method == "POST" and form.validate():
+    user = User.query.filter_by(email = form.email.data).first()
+    if user and bcrypt.check_password_hash(user.password, form.password.data):
+      session['email'] = form.email.data
+      flash(f'Logado com sucesso {form.email.data}','success')
+      return redirect(request.args.get('next') or url_for('admin'))
+    else:
+      flash(f'Email ou senha invalida!!!','danger')
+  return render_template('admin/login.html', form=form, title='Login')
